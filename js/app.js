9c9a9458191c3cc97c3d049ce841f8d635a64599
mrsai <sailoonkyaw2019@gmail.com>
2023-07-13 12:40:13 +0630

    first M

--- js/app.js
+++ js/app.js
@@ -105,6 +105,33 @@ ctx.height = 250;
 
 
 
+// start earning area 
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+// end earning area 
+
+
+
 
 
 // End Js Area 
